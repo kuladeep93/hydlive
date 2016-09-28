388c3ab8e87788cf8f4ecd14ea498347f9ecac86
DESKTOP-NJ8FDQ7\KULADEEP <s.kuladeep93@gmail.com>
2016-09-28 10:49:52 +0530

    slick slider added

--- js/stylesheets.js
+++ js/stylesheets.js
@@ -1,3 +1,14 @@
-
+$(document).ready(function () {
+    $('.slider-img').slick({
+        fade: true,
+        cssEase: 'linear',
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+});
 
 
